54702c228dad38869e2f3ab077e7a9575c205b22
zcf <2764300331@qq.com>
2019-10-16 18:02:02 +0800

    fuck

--- src/main/java/com/maamcare/bmi/controller/UserController.java
+++ src/main/java/com/maamcare/bmi/controller/UserController.java
@@ -14,16 +14,19 @@ public class UserController {
 
     @GetMapping("/noLogin")
     public Result noLogin(HttpSession session){
-        return Result.builder().status(0).err("用户未登录").data(null).build();
+        return Result.builder().status(1).err("用户未登录").data(null).build();
     }
 
-    @GetMapping("/login")
-    public Result login(@RequestParam String a, HttpSession session){
-        session.setAttribute("loginUser",a);
-        return Result.builder().status(0).err("用户已登录").data(a).build();
+    @PostMapping("/login")
+    public Result login(@RequestBody UserFormInfo userFormInfo, HttpSession session){
+        //session.setAttribute("userName","1");
+        System.out.println(userFormInfo.toString());
+        HashMap<String, String> map = new HashMap<>();
+        map.put("userid","曹尼玛傻逼");
+        return Result.builder().status(0).err("").data(map).build();
     }
     @PostMapping("/register")
-    public Result register(@RequestBody UserFormInfo userFormInfo){
+    public Result register(@RequestBody UserFormInfo userFormInfo,HttpSession session){
         System.out.println(userFormInfo.toString());
         HashMap<String, String> map = new HashMap<>();
         map.put("userid","119114");
